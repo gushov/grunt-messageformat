b4ee211a7812fc5f64fe45239279959613d50ccb
Mark Reynolds <mreynolds@homeaway.com>
2014-05-02 11:36:42 -0700

    Upgrade messageformat and match generated formats

--- test/expected/en/i18n.js
+++ test/expected/en/i18n.js
@@ -1,21 +1,14 @@
 (function(){ window.i18n || (window.i18n = {}) 
 var MessageFormat = { locale: {} };
-MessageFormat.locale.en = function ( n ) {
-  if ( n === 1 ) {
-    return "one";
-  }
-  return "other";
-};
-
-window.i18n["greetings"] = {}
-window.i18n["greetings"]["hello"] = function(d){
-var r = "";
-r += "this is my hello message";
-return r;
-}
-window.i18n["greetings"]["goodbye"] = function(d){
-var r = "";
-r += "farewell";
-return r;
-}
+MessageFormat.locale.en=function(n){return n===1?"one":"other"}
+var
+c=function(d){if(!d)throw new Error("MessageFormat: No data passed to function.")},
+n=function(d,k,o){if(isNaN(d[k]))throw new Error("MessageFormat: `"+k+"` isnt a number.");return d[k]-(o||0)},
+v=function(d,k){c(d);return d[k]},
+p=function(d,k,o,l,p){c(d);return d[k] in p?p[d[k]]:(k=MessageFormat.locale[l](d[k]-o),k in p?p[k]:p.other)},
+s=function(d,k,p){c(d);return d[k] in p?p[d[k]]:p.other};
+window.i18n["greetings"] = {
+"hello":function(d){return "this is my hello message"},
+"goodbye":function(d){return "farewell"},
+"myname":function(d){return "my name is "+v(d,"FIRST_NAME")}}
 })();
